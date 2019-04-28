efdc94df75fdb32b0e1237afeaea8e186e9726f6
sarahkirby <sk.irby@icloud.com>
2019-04-28 14:36:46 +0530

    portfolio-site/ Initial setup with create-react-app
    implement absolute paths and install sass

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "components/App/App.scss"
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+        sarakirby.dev
+        <span>Coming soon ...</span>
+      </p>
+    </div>
+  )
+}
+
+export default App
