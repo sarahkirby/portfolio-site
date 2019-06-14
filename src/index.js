4a096a246293f64033fe8c948fbb7d0b8ccb9104
sarahkirby <sk.irby@icloud.com>
2019-06-14 14:28:43 +0100

    Scroll to top of page functionality
    for routing

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Route } from 'react-router-dom';
 import './index.scss'
+import ScrollToTop from 'components/Helpers/ScrollToTop'
 import App from 'pages/app'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
   <BrowserRouter>
-    <Route path="/" component={App} />
+    <ScrollToTop>
+      <Route path="/" component={App} />
+    </ScrollToTop>
   </BrowserRouter>,
 document.getElementById("root"))
 
