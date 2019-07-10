219dea7d491377eca5c4554b2ecb076d27d8de6b
sarahkirby <sk.irby@icloud.com>
2019-07-10 11:59:34 +0100

    Add og:image for link sharing

--- src/assets/images/images.js
+++ src/assets/images/images.js
@@ -2,10 +2,12 @@ import enrichme from 'assets/images/enrichme/index'
 import bcito from 'assets/images/bcito/index'
 import fwaste from 'assets/images/fwaste/index'
 import me from 'assets/images/sarah-kirby.png'
+import ogImage from 'assets/images/og-image.png'
 
 export {
   enrichme,
   bcito,
   fwaste,
   me,
+  ogImage,
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -3,12 +3,14 @@ import {Helmet} from 'react-helmet'
 import {CSSTransition} from 'react-transition-group'
 import LandingPage from 'components/LandingPage/LandingPage'
 import Works from 'components/Works/Works'
+import {ogImage} from 'assets/images/images'
 
 const Home = props => (
   <div className="home fade-in">
     <Helmet>
       <title>Sarah Kirby | Developer & Designer Portfolio</title>
       <meta name="description" content="Sarah Kirby is a front-end developer and designer from New Zealand living in London. She is on the look out for a creative front-end developer role, ideally working with JS frameworks such as React." />
+      <meta property="og:image" content={`sarahkirby.dev${ogImage}`} />
     </Helmet>
 
     <CSSTransition
